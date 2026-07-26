07d272b6f5613819f8f16edc2fbd0f7103165423
mdlr-json <81268762+mdlr-json@users.noreply.github.com>
2026-07-26 11:52:31 -0500

    Update script.js
    Found and fixed issue on line 11:
    - Changed placeholder text for loop index upper bounds to intended length of pages array

--- script.js
+++ script.js
@@ -8,10 +8,11 @@ const titlePlate = () => {
 const navBar = () => {
   let content = '<ul>';
   const pages = ['index'];
-  for (i = 0; i < n; i++) {
+  for (i = 0; i < pages.length; i++) {
     content += (`<li><a src="/${pages[i]}.html">${pages[i]}</a></li>`);
   }
-  return content += '</ul>';
+  content += '</ul>';
+  return content;
 };
 
 document.getElementById('page-title').innerHTML = titlePlate();
